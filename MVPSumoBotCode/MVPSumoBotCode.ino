19df65c6850c98e9a8b39d336e0d8697c986d7a4
Jatin Mathur <jatin.mathur25@gmail.com>
2024-04-18 20:15:17 -0700

    save

--- MVPSumoBotCode/MVPSumoBotCode.ino
+++ MVPSumoBotCode/MVPSumoBotCode.ino
@@ -8,14 +8,28 @@ const int rightReflectancePin = A0; // Right reflectance sensor pin
 
 
 // Define pins for motor control
-const int leftMotorPWM = 5;      // PWM pin for left motor speed control
-const int leftMotorDir1 = 6;    // Direction pin 1 for left motor
-const int leftMotorDir2 = 7;    // Direction pin 2 for left motor
-const int rightMotorPWM = 17;     // PWM pin for right motor speed control
-const int rightMotorDir1 = 9;    // Direction pin 1 for right motor
-const int rightMotorDir2 = 10;    // Direction pin 2 for right motor
-const int standbyPin = 8;       // Standby pin for motor controller
+// Pinout: https://docs.arduino.cc/tutorials/nano-esp32/pin-setup/
+//int ledChannelLeft = 0;
+//int ledChannelRight = 1;
 
+//const int leftMotorPWM = 5;      // PWM pin for left motor speed control
+//const int leftMotorDir1 = 6;    // Direction pin 1 for left motor
+//const int leftMotorDir2 = 7;    // Direction pin 2 for left motor
+
+const int leftMotorPWM = 3;      // PWM pin for left motor speed control
+const int leftMotorDir1 = 4;    // Direction pin 1 for left motor
+const int leftMotorDir2 = 2;    // Direction pin 2 for left motor
+
+//const int rightMotorPWM = 17;     // PWM pin for right motor speed control
+//const int rightMotorDir1 = 9;    // Direction pin 1 for right motor
+//const int rightMotorDir2 = 10;    // Direction pin 2 for right motor
+
+const int rightMotorPWM = 9;     // PWM pin for right motor speed control
+const int rightMotorDir1 = 6;    // Direction pin 1 for right motor
+const int rightMotorDir2 = 7;    // Direction pin 2 for right motor
+
+//const int standbyPin = 8;       // Standby pin for motor controller
+const int standbyPin = 5;       // Standby pin for motor controller
 
 // Define motor speed
 const int moderateSpeed = 200;      // Moderate motor speed
@@ -38,39 +52,49 @@ void setup() {
   pinMode(rightMotorDir2, OUTPUT);
   pinMode(standbyPin, OUTPUT);
 
+//  pinMode(D7, OUTPUT);
+//  pinMode(D6, OUTPUT);
+
   // Setup PWM channels
-  ledcSetup(pwmChannelLeftMotor, 5000, 8);    // 5000 Hz frequency, 8-bit resolution
-  ledcSetup(pwmChannelRightMotor, 5000, 8);   // 5000 Hz frequency, 8-bit resolution
+//  ledcSetup(ledChannelLeft, 5000, 8);    // 5000 Hz frequency, 8-bit resolution
+//  ledcSetup(ledChannelRight, 5000, 8);   // 5000 Hz frequency, 8-bit resolution
 
   // Attach PWM channels to GPIO pins
-  ledcAttachPin(leftMotorPWM, pwmChannelLeftMotor);
-  ledcAttachPin(rightMotorPWM, pwmChannelRightMotor);
+//  ledcAttachPin(ledChannelLeft, leftMotorPWM);
+//  ledcAttachPin(ledChannelRight, rightMotorPWM);
 
   // Enable motor controller
   digitalWrite(standbyPin, HIGH);
 
-  Serial.begin(9600); // Initialize serial communication
+  Serial.begin(115200); // Initialize serial communication
 }
 
 void loop() {
+  Serial.println("yo2");
   motor1.drive(255,1000);
   motor1.drive(-255,1000);
   motor1.brake();
+  motor2.drive(255,1000);
+  motor2.drive(-255,1000);
+  motor2.brake();
+  //digitalWrite(leftMotorPWM, HIGH);
+//  digitalWrite(10, HIGH);
+//  digitalWrite(9, HIGH);
   delay(1000);
 
 }
 
 // Function to measure distance using ultrasonic sensor
-int getDistance() {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  long duration = pulseIn(echoPin, HIGH);
-  int distance = duration * 0.034 / 2;  // Convert time to distance (cm)
-  return distance;
-}
+//int getDistance() {
+//  digitalWrite(trigPin, LOW);
+//  delayMicroseconds(2);
+//  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(10);
+//  digitalWrite(trigPin, LOW);
+//  long duration = pulseIn(echoPin, HIGH);
+//  int distance = duration * 0.034 / 2;  // Convert time to distance (cm)
+//  return distance;
+//}
 
 // Function to move the robot forward
 void moveForward(int speed) {
